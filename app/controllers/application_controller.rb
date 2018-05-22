d521f6dfbcc06c3947ec8a9a63f0865a30a1b35b
Amanda Garrison <amandakgarrison@gmail.com>
2018-05-22 08:34:41 -0400

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+
+	before_action :categories	
+	before_action :authenticate_user!
+
+def categories
+	@categories = Category.order(:name)
+
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'storefront#all_items'
-  get 'category' => 'storefront#items_by_category'
+  devise_for :users
+  root 'storefront#index'
+  get 'store' => 'storefront#all_items'
+  get 'categorical' => 'storefront#items_by_category'
   get 'brand' => 'storefront#items_by_brand'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
